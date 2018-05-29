dca8c368038cc519eb0cf795187fddfe9295601f
shiori.kamo <shiori.kamo@rakuten.com>
2018-05-29 02:46:09 +0000

    start mysql

--- message-board/routes/web.php
+++ message-board/routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'TasksCostroller@index');
 
-Route::resource('tasks', 'TasksController');
+Route::get('/', function () {
+    return view('welcome');
+});
