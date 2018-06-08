1f85508fa581a7645562291b996d905d4a25ce2d
shiori.kamo <shiori.kamo@rakuten.com>
2018-06-08 04:29:08 +0000

    top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>TaskBoard</title>
+        <title>Microposts</title>
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -14,10 +14,10 @@
     </head>
     <body>
         @include('commons.navbar')
-        
+
         <div class="container">
-            @include('commons.error_tasks')
-            
+            @include('commons.error_messages')
+
             @yield('content')
         </div>
     </body>
